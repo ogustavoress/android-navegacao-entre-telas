d2dcfc51ec6981d867b092383dda55850e00aa6a
Gustavo <ogustavoress@gmail.com>
2025-10-13 20:50:42 -0300

    chore: adicionando dependência para suporte à navegação entre telas com Jetpack Compose

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,6 +48,7 @@ dependencies {
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
+    implementation("androidx.navigation:navigation-compose:2.9.50")
     implementation(libs.androidx.compose.material3)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
