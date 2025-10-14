fb3d186a23adc3e1ead7affb8f5dfb73d10fe310
Gustavo <ogustavoress@gmail.com>
2025-10-14 10:12:52 -0300

    feat(NavController): implementação do gerenciador de navegação entre telas com Navigation Compose

--- app/src/main/java/com/ogustavoress/navegacaoentretelas/MainActivity.kt
+++ app/src/main/java/com/ogustavoress/navegacaoentretelas/MainActivity.kt
@@ -8,6 +8,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
 import com.ogustavoress.navegacaoentretelas.screens.LoginScreen
 import com.ogustavoress.navegacaoentretelas.ui.theme.NavegacaoentretelasTheme
 
@@ -18,7 +20,26 @@ class MainActivity : ComponentActivity() {
         setContent {
             NavegacaoentretelasTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    LoginScreen(modifier = Modifier.padding(innerPadding))
+                    val navController = rememberNavController()
+
+                    NavHost(
+                        navController = navController,
+                        startDestination = "login",
+                    )
+                    {
+                        composable(route = "login") {
+                            LoginScreen(modifier = Modifier.padding(innerPadding))
+                        }
+                        composable(route = "menu") {
+                            MenuScreen(modifier = Modifier.padding(innerPadding))
+                        }
+                        composable(route = "pedidos") {
+                            PedidosScreen(modifier = Modifier.padding(innerPadding))
+                        }
+                        composable(route = "perfil") {
+                            PerfilScreen(modifier = Modifier.padding(innerPadding))
+                        }
+                    }
                 }
             }
         }
